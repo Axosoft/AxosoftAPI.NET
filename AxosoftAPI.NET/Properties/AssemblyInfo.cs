c2e44100896751ccb39d1799171612d3aa940447
gusarizona <gusarizona@hotmail.com>
2014-04-17 14:12:00 -0700

    Modified nuget and assembly info to reflect v3 settings.

--- AxosoftAPI.NET/Properties/AssemblyInfo.cs
+++ AxosoftAPI.NET/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AxosoftAPI.NET")]
-[assembly: AssemblyDescription(".NET wrapper for the Axosoft API")]
+[assembly: AssemblyDescription(".NET wrapper for the Axosoft API v3")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Axosoft")]
 [assembly: AssemblyProduct("AxosoftAPI.NET")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
