59a7f738fe03e3602a0a8d0b092681718652ea3f
brettgaxosoft <brettg@axosoft.com>
2015-03-04 08:11:09 -0700

    Added PickListValue class

---
+++ AxosoftAPI.NET/Models/PicklistValue.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace AxosoftAPI.NET.Models
+{
+    public class PicklistValue : BaseModel
+    {
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("order")]
+        public int Order { get; set; }
+
+        [JsonProperty("picklist_id")]
+        public int PicklistId { get; set; }
+    }
+}
