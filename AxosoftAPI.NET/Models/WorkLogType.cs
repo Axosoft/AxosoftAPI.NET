e01ede5583aa2c9f6849b3e7927ed6834c082a05
brettgaxosoft <brettg@axosoft.com>
2015-11-02 07:34:16 -0700

    Added Name to worklog model

--- AxosoftAPI.NET/Models/WorkLogType.cs
+++ AxosoftAPI.NET/Models/WorkLogType.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace AxosoftAPI.NET.Models
 {
 	public class WorkLogType : BaseModel
 	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
 	}
 }
