ec5db9fed8eee27d4a4331fa222eb69d91d27645
Brett Goldman <brettg@axosoft.com>
2016-12-29 15:49:55 -0700

    Added Capacity data

--- AxosoftAPI.NET/Models/Release.cs
+++ AxosoftAPI.NET/Models/Release.cs
@@ -41,5 +41,24 @@ namespace AxosoftAPI.NET.Models
 
 		[JsonProperty("children")]
 		public IEnumerable<Release> SubReleases { get; set; }
+
+		[JsonProperty("capacity")]
+		public DurationUnit Capacity { get; set; }
+
+		[JsonProperty("total_capacity_min")]
+		public double? TotalCapacity { get; set; }
+
+		[JsonProperty("original_estimate_min")]
+		public double? OriginalEstimateMin { get; set; }
+
+		[JsonProperty("remaining_estimate_min")]
+		public double? RemainingEstimateMin { get; set; }
+
+		[JsonProperty("work_logged_min")]
+		public double? WorkLoggedMin { get; set; }
+
+		[JsonProperty("completion_date")]
+		public DateTime? CompletionDate { get; set; }
+
 	}
 }
